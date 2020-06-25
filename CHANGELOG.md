ffafcf5ba4202bd87606cde27cdc64875a557ccf
DarrenPaulWright <darren@darrenpaulwright.com>
2020-06-25 10:53:37 -0500

    Full test coverage & changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,12 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html)
 
+## [0.2.0] - 2020-06-25
+### Added
+- settings.params option
+
 ## 0.1.0 - 2020-06-24
 ### Added
-- prioritize
+- Initial code
+
+[0.2.0]: https://github.com/DarrenPaulWright/prioritize/compare/v0.1.0...0.2.0
--- tests/prioritize.test.js
+++ tests/prioritize.test.js
@@ -1,5 +1,32 @@
+import { resolveAfterWith } from 'async-agent';
 import { assert } from 'type-enforcer';
 import prioritize from '../src/prioritize.js';
+
+class FetchMock {
+	constructor(onFetch) {
+		const self = this;
+
+		self._originalFetch = window.fetch;
+
+		window.fetch = (url, settings) => {
+			self.url = url;
+			self.settings = settings;
+
+			if (onFetch) {
+				onFetch(url, settings);
+			}
+			else {
+				self.reset();
+			}
+
+			return resolveAfterWith(20, 'response')();
+		};
+	}
+
+	reset() {
+		window.fetch = this._originalFetch;
+	}
+}
 
 describe('prioritize', () => {
 	it('should have property baseUrl', () => {
@@ -8,5 +35,85 @@ describe('prioritize', () => {
 
 	it('should have default settings', () => {
 		assert.equal(prioritize.defaults, { headers: { 'Content-Type': 'application/json' } });
+	});
+
+	it('should wait to call if priority is low', (done) => {
+		const allUrls = [];
+
+		const mock = new FetchMock((url) => {
+			allUrls.push(url.pathname);
+		});
+
+		prioritize.get('/first');
+		prioritize.get('/second', { priority: 'low' })
+			.then((response) => {
+				assert.is(response, 'response');
+				assert.equal(allUrls, ['/first', '/third', '/second']);
+				mock.reset();
+				done();
+			});
+		prioritize.get('/third');
+	});
+
+	describe('shortcuts', () => {
+		it('should add appropriate method when get is called', () => {
+			const mock = new FetchMock();
+
+			return prioritize.get('/api/v1', { params: { a: 1, b: 2 } })
+				.then((response) => {
+					assert.is(response, 'response');
+					assert.is(mock.url.pathname, '/api/v1');
+					assert.is(mock.url.search, '?a=1&b=2');
+					assert.is(mock.settings.method, 'GET');
+				});
+		});
+
+		it('should add appropriate method when patch is called', () => {
+			const mock = new FetchMock();
+
+			return prioritize.patch('/api/v1', { body: { a: 1 } })
+				.then((response) => {
+					assert.is(response, 'response');
+					assert.is(mock.url.pathname, '/api/v1');
+					assert.is(mock.settings.body, '{"a":1}');
+					assert.is(mock.settings.method, 'PATCH');
+				});
+		});
+
+		it('should add appropriate method when put is called', () => {
+			const mock = new FetchMock();
+
+			return prioritize.put('/api/v1', { body: { a: 1 } })
+				.then((response) => {
+					assert.is(response, 'response');
+					assert.is(mock.url.pathname, '/api/v1');
+					assert.is(mock.settings.body, '{"a":1}');
+					assert.is(mock.settings.method, 'PUT');
+				});
+		});
+
+		it('should add appropriate method when post is called', () => {
+			const mock = new FetchMock();
+
+			return prioritize.post('/api/v1', { body: { a: 1 } })
+				.then((response) => {
+					assert.is(response, 'response');
+					assert.is(mock.url.pathname, '/api/v1');
+					assert.is(mock.settings.body, '{"a":1}');
+					assert.is(mock.settings.method, 'POST');
+				});
+		});
+
+		it('should add appropriate method when delete is called', () => {
+			const mock = new FetchMock();
+
+			return prioritize.delete('/api/v1', { body: { a: 1 } })
+				.then((response) => {
+					assert.is(response, 'response');
+					assert.is(mock.url.pathname, '/api/v1');
+					assert.is(mock.settings.body, '{"a":1}');
+					assert.is(mock.settings.method, 'DELETE');
+				});
+		});
 	});
 });
